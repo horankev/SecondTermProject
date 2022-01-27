7db9e51d0311b3143cda838ccc3449ac65093354
horankev <horank@gmail.com>
2022-01-27 18:54:42 +0000

    pca in various displays

---
+++ code/pca.R
@@ -0,0 +1,96 @@
+library(tidyverse)
+library(sf)
+library(parlitools)
+library(GGally)
+library(patchwork)
+library(lme4)
+library(mgcv)
+library(ggfortify)
+library(here)
+
+rm(list=ls())
+here()
+
+#################################
+
+# read in required data
+census_reduced <- readRDS(here("data", "census_reduced.rds"))
+comb2019_reduced <- readRDS(here("data", "comb2019_reduced.rds"))
+party_colour <- readRDS(here("data", "party_colour.rds"))
+comb2019 <- readRDS(here("data", "comb2019.rds"))
+comb2017 <- readRDS(here("data", "comb2017.rds"))
+comb2015 <- readRDS(here("data", "comb2015.rds"))
+
+
+mycolours_party <- party_colour$party_colour
+names(mycolours_party) <- party_colour$party_name
+colscale_party2019 <- scale_colour_manual(names(mycolours_party), values=mycolours_party, breaks=unique(comb2019$winner_19))
+colscale_party2017 <- scale_colour_manual(names(mycolours_party), values=mycolours_party, breaks=unique(comb2017$winner_17)) 
+colscale_party2015 <- scale_colour_manual(names(mycolours_party), values=mycolours_party, breaks=unique(comb2015$winner_15))
+fillscale_party2019 <- scale_fill_manual(names(mycolours_party), values=mycolours_party, breaks=unique(comb2019$winner_19))
+fillscale_party2017 <- scale_fill_manual(names(mycolours_party), values=mycolours_party, breaks=unique(comb2017$winner_17)) 
+fillscale_party2015 <- scale_fill_manual(names(mycolours_party), values=mycolours_party, breaks=unique(comb2015$winner_15))
+mycolours_country <- c("red", "blue4", "#008142")
+names(mycolours_country) <- c("England", "Scotland", "Wales")
+colscale_country <- scale_colour_manual(names(mycolours_country), values=mycolours_country)
+fillscale_country <- scale_fill_manual(names(mycolours_country), values=mycolours_country)
+#################################
+
+# PCA
+
+# calculate principal components
+PCAresults <- prcomp(census_reduced %>% select(-pano), scale. = TRUE)
+
+# biplot coloured by country
+autoplot(PCAresults, data = comb2019_reduced, geom = 'point', colour = 'country', size=2, loadings = TRUE, 
+         loadings.colour = 'blue', 
+         loadings.label = TRUE, loadings.label.size = 4, loadings.label.colour = "black", loadings.label.hjust = -0.15) + 
+  colscale_country + 
+  theme(plot.background=element_blank(),
+        panel.background=element_rect(fill='transparent', color='black',size=1),
+        legend.text=element_text(hjust=1),
+        legend.key=element_blank())
+
+# biplot coloured by country, faceted by country
+autoplot(PCAresults, data = comb2019_reduced, geom = 'point', colour = 'country', size=2, loadings = TRUE, 
+         loadings.colour = 'blue', 
+         loadings.label = TRUE, loadings.label.size = 4, loadings.label.colour = "black", loadings.label.hjust = -0.15) + 
+  colscale_country + 
+  theme(plot.background=element_blank(),
+        panel.background=element_rect(fill='transparent', color='black',size=1),
+        legend.text=element_text(hjust=1),
+        legend.key=element_blank()) + 
+  facet_wrap(~country)
+
+# biplot coloured by winner
+autoplot(PCAresults, data = comb2019_reduced, geom = 'point', colour = 'winner_19', size=1, loadings = TRUE,
+         loadings.colour = 'blue',
+         loadings.label = TRUE, loadings.label.size = 4, loadings.label.colour = "black", loadings.label.hjust = -0.15) + 
+  colscale_party2019 + 
+  theme(plot.background=element_blank(),
+        panel.background=element_rect(fill='transparent', color='black',size=2),
+        legend.text=element_text(hjust=1),
+        legend.key=element_blank())
+
+# biplot coloured by winner, faceted by country
+autoplot(PCAresults, data = comb2019_reduced, geom = 'point', colour = 'winner_19', size=1, loadings = TRUE,
+         loadings.colour = 'blue',
+         loadings.label = TRUE, loadings.label.size = 4, loadings.label.colour = "black", loadings.label.hjust = -0.15) + 
+  colscale_party2019 + 
+  theme(plot.background=element_blank(),
+        panel.background=element_rect(fill='transparent', color='black',size=2),
+        legend.text=element_text(hjust=1),
+        legend.key=element_blank()) + 
+  facet_wrap(~country)
+
+# biplot coloured by winner, faceted by region
+autoplot(PCAresults, data = comb2019_reduced, geom = 'point', colour = 'winner_19', size=2, loadings = TRUE,
+         loadings.colour = 'black',
+         loadings.label = TRUE, loadings.label.size = 4, loadings.label.colour = "black", loadings.label.hjust = -0.15) + 
+  colscale_party2019 + 
+  theme(plot.background=element_blank(),
+        panel.background=element_rect(fill='transparent', color='black',size=2),
+        legend.text=element_text(hjust=1),
+        legend.key=element_blank()) + 
+  facet_wrap(~region)
+
