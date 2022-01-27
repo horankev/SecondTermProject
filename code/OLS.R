20946b0765c59446b9765dc961a080ec76f539cf
horankev <horank@gmail.com>
2022-01-27 23:14:51 +0000

    tidied

--- code/OLS.R
+++ code/OLS.R
@@ -14,28 +14,25 @@ here()
 #################################
 
 # read in required data
-comb2019_reduced <- readRDS(here("data", "comb2019_reduced.rds"))
-comb2017_reduced <- readRDS(here("data", "comb2017_reduced.rds"))
-comb2015_reduced <- readRDS(here("data", "comb2015_reduced.rds"))
-
+all_elections_reduced <- readRDS(here("data", "all_elections_reduced.rds"))
 
 #################################
 # OLS for conservative %, labour %, libdem %
 reg_cons <- lm(con_19 ~ leave_hanretty + age_18_44 + age_45_64 + cars_none +
                  qual_none + health_bad_both + deprived_none + house_owned +
                  household_one_person + ethnicity_white + born_uk + christian +
-                 unemployed + retired + born_elsewhere, data=comb2019_reduced)
+                 unemployed + retired + born_elsewhere, data=all_elections_reduced)
 summary(reg_cons)
 
 reg_lab <- lm(lab_19 ~ leave_hanretty + age_18_44 + age_45_64 + cars_none +
                 qual_none + health_bad_both + deprived_none + house_owned +
                 household_one_person + ethnicity_white + born_uk + christian +
-                unemployed + retired + born_elsewhere, data=comb2019_reduced)
+                unemployed + retired + born_elsewhere, data=all_elections_reduced)
 summary(reg_lab)
 
 reg_ld <- lm(ld_19 ~ leave_hanretty + age_18_44 + age_45_64 + cars_none +
                qual_none + health_bad_both + deprived_none + house_owned +
                household_one_person + ethnicity_white + born_uk + christian +
-               unemployed + retired + born_elsewhere, data=comb2019_reduced)
+               unemployed + retired + born_elsewhere, data=all_elections_reduced)
 summary(reg_ld)
 
